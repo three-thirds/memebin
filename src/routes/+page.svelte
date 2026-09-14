07ec3cf306a11af129056aa1bfa85ec83c156365
William Gu <williamnumismatics@gmail.com>
2026-09-14 20:29:56 +1000

    got the shadcn svelte working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
+ import { Button } from "$lib/components/ui/button/index.js";
 </script>
-
-<Button>Styled button</Button>
+ 
+<div class="flex flex-col items-start gap-8 sm:flex-row">
+ <div class="flex items-start gap-2">
+  <Button size="sm" variant="outline">Small</Button>
+  <Button size="icon-sm" aria-label="Submit" variant="outline">
+  </Button>
+ </div>
+ <div class="flex items-start gap-2">
+  <Button variant="outline">Default</Button>
+  <Button size="icon" aria-label="Submit" variant="outline">
+  </Button>
+ </div>
+ <div class="flex items-start gap-2">
+  <Button variant="outline" size="lg">Large</Button>
+  <Button size="icon-lg" aria-label="Submit" variant="outline">
+  </Button>
+ </div>
+</div>
