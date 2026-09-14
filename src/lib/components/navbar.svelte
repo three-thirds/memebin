f2d9e5e23bb1b2c3d88e5d6887b0aba1fd2d3b30
William Gu <williamnumismatics@gmail.com>
2026-09-14 20:46:46 +1000

    changed navbar buttons to a const and a for each loop

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,3 +1,11 @@
-<button>Upload</button>
-<button>Browse</button>
-<button>Docs</button>
+<script lang="ts">
+    export const NavButtons = [
+        {href: "https://google.com", text: "Upload"},
+        {href: "google.com", text: "Upload"},
+        {href: "google.com", text: "Upload"}
+    ]
+</script>
+
+{#each NavButtons as button}
+    <a href={button.href} class="text-white">{button.text}</a>
+{/each}
