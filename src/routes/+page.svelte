7fce486bb1f8c363fc0a7daa3509d38f622d9059
William Gu <williamnumismatics@gmail.com>
2026-09-15 21:04:16 +1000

    funtion so where if u click on the image, it enlarges :p

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { Input } from "$lib/components/ui/input/index.js";
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+    import { buttonVariants } from "$lib/components/ui/button/index.js";
 
     export const Example = [
         {
@@ -37,7 +39,29 @@
 <div class="grid gap-4 pt-8">
 {#each Example as example}
 <div class="flex flex-col gap-2 rounded-lg border p-4">
-    <img src={example.image} alt={example.name} class="text-sm font-medium leading-none w-24 h-24 object-cover" />
+    <Dialog.Root>
+        <Dialog.Trigger>
+            <img src={example.image} alt={example.name} class="text-sm font-medium leading-none w-24 h-24 object-cover" />
+        </Dialog.Trigger>
+        <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+                <Dialog.Header>
+                    <Dialog.Title>{example.name}</Dialog.Title>
+                    <Dialog.Description>
+                        Your Image up close :p
+                    </Dialog.Description>
+                </Dialog.Header>
+                <img src={example.image} alt={example.name} class="text-sm font-medium leading-none w-48 h-48 object-cover" />
+                <Dialog.Footer class="sm:justify-start">
+                    <Dialog.Close class={buttonVariants({ variant: "secondary" })}>
+                        Close
+                    </Dialog.Close>
+                </Dialog.Footer>
+            </Dialog.Content>
+        </Dialog.Portal>
+    </Dialog.Root>
+    
     <!-- <p class="text-sm text-muted-foreground">{example.image}</p> -->
 </div>
 {/each}
