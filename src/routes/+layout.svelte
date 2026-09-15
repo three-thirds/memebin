fc836453c159c30f9f244657c9431bf89a84174e
William Gu <williamnumismatics@gmail.com>
2026-09-15 20:15:47 +1000

    removed navbar not neaded anymreo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <div class="min-h-screen flex flex-col bg-background">
-    <Navbar />
-    <main class="flex-1 bg-background">
+    <!-- <Navbar /> -->
+    <main class="flex-1 bg-background p-3">
         {@render children()}
     </main>
     <Footer />
