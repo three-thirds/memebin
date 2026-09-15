9ad6250abdcb886bf059a9ab8a234e23b0d1a952
William Gu <williamnumismatics@gmail.com>
2026-09-15 20:54:11 +1000

    added basic image showing. @DevaanshPathak is gonna replace example with acual stuff later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
-      import { Input } from "$lib/components/ui/input/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+
+    export const Example = [
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        },
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        },
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        },
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        },
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        },
+        {
+            image: "Hello there", 
+            name: "Hehe"
+        }
+
+    ]
 </script>
  
 <div class="flex flex-col items-start gap-8 sm:flex-row">
 <Input placeholder="Search for anything..." class="rounded-md"/>
+</div>
+
+<div class="grid gap-4 pt-8">
+{#each Example as example}
+<div class="flex flex-col gap-2 rounded-lg border p-4">
+    <p class="text-sm font-medium leading-none">{example.name}</p>
+    <p class="text-sm text-muted-foreground">{example.image}</p>
+</div>
+{/each}
 </div>
