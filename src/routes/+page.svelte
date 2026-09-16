d40d3725b11cc99e86fa7d22fe141e0b56981677
William Gu <williamnumismatics@gmail.com>
2026-09-16 20:33:09 +1000

    added tags function and also enlargened the page size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,57 +2,57 @@
     import { Input } from "$lib/components/ui/input/index.js";
     import * as Dialog from "$lib/components/ui/dialog/index.js";
     import { buttonVariants } from "$lib/components/ui/button/index.js";
+    import { IconTag } from '@tabler/icons-svelte-runes';
 
     export const Example = [
         {
             image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
+            name: "Diet Lmao",
+            uses: 6,
+            tag: "Group Chat"
         },
         {
             image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
+            name: "Diet Lmao",
+            uses: 6,
+            tag: "Group Chat"
         },
         {
             image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
+            name: "Diet Lmao",
+            uses: 6,
+            tag: "Group Chat"
         },
-        {
-            image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
-        },
-        {
-            image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
-        },
-        {
-            image: "https://cdn.hackclub.com/01a0a4b5-fc23-7376-beb8-64e977a42f13/image.png", 
-            name: "Hehe"
-        }
 
     ]
 </script>
  
-<div class="flex flex-col items-start gap-8 sm:flex-row">
+<div class="flex flex-col items-stretch gap-8 sm:flex-row">
 <Input placeholder="Search for anything..." class="rounded-md"/>
 </div>
 
-<div class="grid gap-4 pt-8">
+<div class="grid grid-cols-2 gap-4 pt-8">
 {#each Example as example}
-<div class="flex flex-row gap-2 rounded-lg border p-4 border-border bg-background/50 hover:bg-background transition-colors">
+<div class="flex flex-row gap-2 rounded-lg border p-4 border-border bg-background/50 hover:bg-background transition-colors min-w-0">
     <Dialog.Root>
-        <Dialog.Trigger class="inline-block w-fit">
-            <img src={example.image} alt={example.name} class="tw-24 h-24 object-cover rounded-md ring-1 ring-border transition-all duration-200 hover:ring-2 hover:ring-accent-foreground hover:scale-105 hover:brightness-110 cursor-pointer" />
+        <Dialog.Trigger class="inline-block w-fit shrink-0">
+            <img src={example.image} alt={example.name} class="w-48 h-full object-cover rounded-md ring-1 ring-border transition-all duration-200 hover:ring-2 hover:ring-accent-foreground hover:scale-105 hover:brightness-110 cursor-pointer" />
         </Dialog.Trigger>
         <Dialog.Portal>
             <Dialog.Overlay />
-            <Dialog.Content>
+            <Dialog.Content class="flex flex-col gap-4 p-6 px-6 pb-8">
                 <Dialog.Header>
                     <Dialog.Title>{example.name}</Dialog.Title>
                     <Dialog.Description>
-                        Your Image up close :p
+                        Your Image & Tags
                     </Dialog.Description>
                 </Dialog.Header>
-                <img src={example.image} alt={example.name} class="text-sm font-medium leading-none w-48 h-48 object-cover" />
+                <img src={example.image} alt={example.name} class="text-sm font-medium leading-none w-48 h-48 object-cover rounded-md" />
+                <p 
+                class="flex max-w-full min-w-0 items-center gap-1.5 overflow-hidden rounded-2xl border border-border bg-secondary/60 px-2 py-1 text-xs font-medium text-secondary-foreground hover:bg-secondary cursor-pointer">
+                    <IconTag stroke={2} size={16.7} class="shrink-0"/>
+                    <kbd class="min-w-0 truncate">{example.tag}</kbd>
+                </p>
                 <Dialog.Footer class="sm:justify-start">
                     <Dialog.Close class={buttonVariants({ variant: "secondary" })}>
                         Close
@@ -61,7 +61,18 @@
             </Dialog.Content>
         </Dialog.Portal>
     </Dialog.Root>
-    Hi
+    
+    <div class="flex min-w-0 flex-col gap-1 items-start justify-between border-l border-dashed p-2">
+        <div>
+            <h3 class="text-sm font-medium leading-none">{example.name}</h3>
+            <h4 class="text-xs text-muted-foreground"><b>Uses:</b> {example.uses}</h4>
+        </div>
+        <p 
+        class="flex max-w-full min-w-0 items-center gap-1.5 overflow-hidden rounded-2xl border border-border bg-secondary/60 px-2 py-1 text-xs font-medium text-secondary-foreground hover:bg-secondary cursor-pointer">
+            <IconTag stroke={2} size={16.7} class="shrink-0"/>
+            <kbd class="max-w-16 truncate">{example.tag}</kbd>
+        </p>
+    </div>
 </div>
 {/each}
 </div>
