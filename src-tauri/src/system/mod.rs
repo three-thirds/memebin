6102c2bc0097a064cdee2a648f307a594cd69e86
Chinmay Shet <chishxd@gmail.com>
2026-09-17 19:24:32 +0530

    feat(tray): implement native system tray with toggle and graceful exit

--- src-tauri/src/system/mod.rs
+++ src-tauri/src/system/mod.rs
@@ -1,4 +1,5 @@
 pub mod clipboard;
+pub mod tray;
 pub mod window;
 
 use std::sync::Mutex;
@@ -8,6 +9,8 @@ use tauri::{App, Manager};
 
 pub fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     window::setup_shortcuts(app)?;
+
+    tray::setup_tray(app)?;
 
     let clipboard = Clipboard::new()?;
     app.manage(Mutex::new(clipboard));
---
+++ src-tauri/src/system/tray.rs
@@ -0,0 +1,57 @@
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    App, Manager,
+};
+
+pub fn setup_tray(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    let show_item = MenuItem::with_id(app, "toggle", "Toggle Memebin", true, None::<&str>)?;
+    let quit_item = MenuItem::with_id(app, "quit", "Quit Memebin", true, None::<&str>)?;
+
+    let menu = Menu::with_items(app, &[&show_item, &quit_item])?;
+
+    let icon = app.default_window_icon().unwrap().clone();
+
+    let _tray = TrayIconBuilder::new()
+        .icon(icon)
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "toggle" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    if window.is_visible().unwrap_or(false) {
+                        let _ = window.hide();
+                    } else {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+            }
+            "quit" => {
+                app.exit(0);
+            }
+            _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+            println!("[DEBUG TRAY EVENT]: {:?}", event); // <--- ADD THIS LINE
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    if window.is_visible().unwrap_or(false) {
+                        let _ = window.hide();
+                    } else {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+            }
+        })
+        .build(app)?;
+
+    Ok(())
+}
