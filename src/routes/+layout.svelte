0c2ce12e8903193c9784b31da8f2c0579ef6cbf2
William Gu <williamnumismatics@gmail.com>
2026-09-20 15:42:36 +1000

    added the mode watcher stuff to layout so it acually works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
     let { children } = $props();
     import Navbar from "$lib/components/navbar.svelte";
     import Footer from "$lib/components/footer.svelte";
+
+    import {ModeWatcher} from "mode-watcher";
 </script>
 
 <div class="relative flex min-h-screen flex-col overflow-hidden rounded-2xl border border-border bg-background/30 backdrop-blur-md">
+    <ModeWatcher />
     <!-- <Navbar /> -->
     <main class="flex-1 bg-background p-3 pb-15">
         {@render children()}
