813bf20529ce882e533ad1f3609012a803c35404
William Gu <williamnumismatics@gmail.com>
2026-09-20 16:12:45 +1000

    got the change menu looking a bity better with shadn Input

--- src/lib/components/dialogs/changemenu.svelte
+++ src/lib/components/dialogs/changemenu.svelte
@@ -2,14 +2,13 @@
     import * as Dialog from "$lib/components/ui/dialog/index.js";
     import {buttonVariants } from "$lib/components/ui/button/index.js";
     import { IconTag } from '@tabler/icons-svelte-runes';
+    import { Input } from "$lib/components/ui/input/index.js";
     
     let {open= $bindable(false), image, name, tag} = $props();
 </script>
 
 <Dialog.Root bind:open>
-    <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content class="flex flex-col gap-4 p-6 px-6 pb-8">
+        <Dialog.Content class="sm:max-w-lg">
             <Dialog.Header>
                 <Dialog.Title>Edit</Dialog.Title>
                 <Dialog.Description>
@@ -22,14 +21,13 @@
                 <IconTag stroke={2} size={16.7} class="shrink-0"/>
                 <kbd class="min-w-0 truncate">{tag}</kbd>
             </p>
-            <input placeholder="Name" class="rounded-md border-2 p-2 min-w-0"/>
-            <input placeholder="Description" class="rounded-md border-2 p-2 min-w-0"/>
-            <input placeholder="Edit Tag" class="rounded-md border-2 p-2 min-w-0"/>
+            <Input placeholder="Name" />
+            <Input placeholder="Description" />
+            <Input placeholder="Edit Tag" />
             <Dialog.Footer class="sm:justify-start">
                 <Dialog.Close class={buttonVariants({ variant: "secondary" })}>
                     Close
                 </Dialog.Close>
             </Dialog.Footer>
         </Dialog.Content>
-    </Dialog.Portal>
 </Dialog.Root>
